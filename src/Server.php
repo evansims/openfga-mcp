2e0fb9c902c87ca35ddd8b8a3eb9115308fe93ae
Evan Sims <hello@evansims.com>
2025-07-10 23:38:47 -0500

    feat: support OPENFGA_MCP_API_TOKEN, OPENFGA_MCP_API_CLIENT_ID, etc.

--- src/Server.php
+++ src/Server.php
@@ -5,12 +5,33 @@ declare(strict_types=1);
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use OpenFGA\Client;
+use OpenFGA\Authentication\{TokenAuthentication, ClientCredentialAuthentication};
 use PhpMcp\Server\Defaults\BasicContainer;
 use PhpMcp\Server\Server;
 use PhpMcp\Server\Transports\{StdioServerTransport, StreamableHttpServerTransport};
 
 try {
-    $openfga = new Client(url: getConfiguredString('OPENFGA_MCP_API_URL', 'http://127.0.0.1:8080'));
+    $authentication = null;
+
+    if (getConfiguredString('OPENFGA_MCP_API_TOKEN', '') !== '') {
+        $authentication = new TokenAuthentication(
+            token: getConfiguredString('OPENFGA_MCP_API_TOKEN', ''),
+        );
+    }
+
+    if (getConfiguredString('OPENFGA_MCP_API_CLIENT_ID', '') !== '') {
+        $authentication = new ClientCredentialAuthentication(
+            clientId: getConfiguredString('OPENFGA_MCP_API_CLIENT_ID', ''),
+            clientSecret: getConfiguredString('OPENFGA_MCP_API_CLIENT_SECRET', ''),
+            issuer: getConfiguredString('OPENFGA_MCP_API_ISSUER', ''),
+            audience: getConfiguredString('OPENFGA_MCP_API_AUDIENCE', ''),
+        );
+    }
+
+    $openfga = new Client(
+        url: getConfiguredString('OPENFGA_MCP_API_URL', 'http://127.0.0.1:8080'),
+        authentication: $authentication,
+    );
 
     $container = new BasicContainer;
     $container->set(Client::class, $openfga);

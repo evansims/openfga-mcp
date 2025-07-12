513bf459bebf116536866be40a3cce29963cf82b
Evan Sims <hello@evansims.com>
2025-07-12 12:27:04 -0500

    docs: documentation improvements

--- src/Server.php
+++ src/Server.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-require_once __DIR__ . '/helpers.php';
+require_once __DIR__ . '/Helpers.php';
 
 use OpenFGA\Authentication\{ClientCredentialAuthentication, TokenAuthentication};
 use OpenFGA\{Client, ClientInterface};
--- tests/Unit/ServerTest.php
+++ tests/Unit/ServerTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-require_once __DIR__ . '/../../src/helpers.php';
+require_once __DIR__ . '/../../src/Helpers.php';
 
 afterEach(function (): void {
     // Clean up environment variables

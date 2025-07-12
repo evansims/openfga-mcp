89671f400f0124e65a10828d53d31d2205a5ee25
Evan Sims <hello@evansims.com>
2025-07-12 12:29:29 -0500

    ci: fix import typo

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-require_once __DIR__ . '/../src/helpers.php';
+require_once __DIR__ . '/../src/Helpers.php';
 
 // If running integration tests (detected by environment or by checking if we have OPENFGA_MCP_API_URL),
 // include the integration bootstrap
